8261f1a0b7e89561e38313fadcf6601806d23d3b
Gekyume <contatogekyume@gmail.com>
2024-09-04 19:26:09 -0300

    delay-exibicao

--- geky.js
+++ geky.js
@@ -11,79 +11,89 @@ const arrayTarefas = new Map();
 let id = 1;
 
 function menuText() {
-    let margem = 7, result;
-    result = "-".repeat(margem) + " MENU PRINCIPAL " + "-".repeat(margem) + "\n";
-    result += "|[1] - Adicionar tarefa      |\n";
-    result += "|[2] - Atualizar tarefa      |\n";
-    result += "|[3] - Deletar tarefa        |\n";
-    result += "|[4] - Obter todas as tarefas|\n";
-    result += "|[5] - Obter uma tarefa      |\n";
-    result += "|[0] - Sair                  |\n";
-    result += "|" + "-".repeat(margem*4) + "|\n\n";
-    return result;
+  let margem = 7,
+    result;
+  result = "-".repeat(margem) + " MENU PRINCIPAL " + "-".repeat(margem) + "\n";
+  result += "|[1] - Adicionar tarefa      |\n";
+  result += "|[2] - Atualizar tarefa      |\n";
+  result += "|[3] - Deletar tarefa        |\n";
+  result += "|[4] - Obter todas as tarefas|\n";
+  result += "|[5] - Obter uma tarefa      |\n";
+  result += "|[0] - Sair                  |\n";
+  result += "|" + "-".repeat(margem * 4) + "|\n\n";
+  return result;
 }
 
 function escolherOpc(opc) {
-    switch (opc) {
-        case 1:
-            adicionarTarefa(arrayTarefas)
-            break;
+  switch (opc) {
+    case 0:
+        console.log("Programa Encerrado!");
+      break;
+    case 1:
+      adicionarTarefa(arrayTarefas);
+      console.log("Tarefa Cadastrada!\n\n");
 
-        case 2:
-            console.log("Atualizar tarefa");
-            break;
+      break;
 
-        case 3:
-            deleteTarefa(arrayTarefas);
-            break;
+    case 2:
+      console.log("Atualizar tarefa");
+      break;
 
-        case 4:
-            console.log("Obter todas as tarefas");
-            console.log(arrayTarefas);
-            //fazer a função com exibição melhorada
-            break;
+    case 3:
+      deleteTarefa(arrayTarefas);
+      break;
 
-        case 5:
-            const idTarefa = prompt.questionInt("Digite o ID da tarefa que sera exibida: ");
-            console.log(arrayTarefas.get(idTarefa))
-            //fazer a função com exibição melhorada
-            break;
+    case 4:
+      console.log("Obter todas as tarefas");
+      console.log(arrayTarefas);
+      //fazer a função com exibição melhorada
+      break;
 
-        default:
-            console.log("Opção inválida. Tente novamente.");
-    }
+    case 5:
+      const idTarefa = prompt.questionInt(
+        "Digite o ID da tarefa que sera exibida: "
+      );
+      console.log(arrayTarefas.get(idTarefa));
+      //fazer a função com exibição melhorada
+      break;
+
+    default:
+      console.log("Opção inválida. Tente novamente.");
+  }
 }
-
 
 let option;
 
-do{
-    console.log(menuText());
-    option = prompt.questionInt("Escolha uma opcao: ");
-    escolherOpc(option)
-}while(option !== 0)
+const pausa = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+do {
+  console.log(menuText());
+  option = prompt.questionInt("Escolha uma opcao: ");
+  escolherOpc(option);
+  await pausa(750);
+} while (option !== 0);
 
-function adicionarTarefa(arrayTarefas){
-    
-    let title = prompt.question("Digite o titulo da Tarefa: ");
+function adicionarTarefa(arrayTarefas) {
+  let title = prompt.question("Digite o titulo da Tarefa: ");
 
-    while (verificarTitulo(arrayTarefas, title)) {
-        console.log("Uma tarefa com esse título já existe. Por favor, escolha um título diferente.");
-        title = prompt.question("Digite o título da Tarefa: ");
-    }
+  while (verificarTitulo(arrayTarefas, title)) {
+    console.log(
+      "Uma tarefa com esse título já existe. Por favor, escolha um título diferente."
+    );
+    title = prompt.question("Digite o título da Tarefa: ");
+  }
 
-    const description = prompt.question("Digite a descrição da Tarefa: ");
-    const status = prompt.question("Digite o status da Tarefa: ");
-    arrayTarefas.set(id, {title, description, status});
-    id++;
+  const description = prompt.question("Digite a descrição da Tarefa: ");
+  const status = prompt.question("Digite o status da Tarefa: ");
+  arrayTarefas.set(id, { title, description, status });
+  id++;
 }
 
 function verificarTitulo(arrayTarefas, title) {
-    for (let tarefa of arrayTarefas.values()) {
-        if (tarefa.title === title) {
-            return true;
-        }
+  for (let tarefa of arrayTarefas.values()) {
+    if (tarefa.title === title) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
