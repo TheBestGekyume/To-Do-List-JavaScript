f7b6784156ecee3216964d3ffe3fc8b1723de951
yagolc <yagolimacapistrano@alu.ufc.br>
2024-09-06 21:12:01 -0300

    funçãoPegarUmaTarefa

--- yago.js
+++ yago.js
@@ -1,57 +1,45 @@
 import prompt from "readline-sync";
+import { exec } from 'child_process';
+exec('chcp 65001'); //fix readline-sync UTF-8
 
-
-const arrayTarefas = new Map();
-
+let taskList = new Map();
 let id = 1;
 
 function menuText() {
-  let margem = 7,
+  let margin = 7,
     result;
-  result = "-".repeat(margem) + " MENU PRINCIPAL " + "-".repeat(margem) + "\n";
+  result = "-".repeat(margin) + " MENU PRINCIPAL " + "-".repeat(margin) + "\n";
   result += "|[1] - Adicionar tarefa      |\n";
   result += "|[2] - Atualizar tarefa      |\n";
   result += "|[3] - Deletar tarefa        |\n";
   result += "|[4] - Obter todas as tarefas|\n";
   result += "|[5] - Obter uma tarefa      |\n";
   result += "|[0] - Sair                  |\n";
-  result += "|" + "-".repeat(margem * 4) + "|\n\n";
+  result += "|" + "-".repeat(margin * 4) + "|\n\n";
   return result;
 }
 
-function escolherOpc(opc) {
-  switch (opc) {
+function chooseOption(option) {
+  switch (option) {
     case 0:
-        console.log("Programa Encerrado!");
+      console.log("Programa Encerrado!");
       break;
     case 1:
-      adicionarTarefa(arrayTarefas);
-      console.log("Tarefa Cadastrada!\n\n");
-
+      addTask(taskList);
+      console.log("Tarefa Adicionada!\n\n");
       break;
-
     case 2:
-      editarTarefa(arrayTarefas);
+      updateTask(taskList);
       break;
-
     case 3:
-      deleteTarefa(arrayTarefas);
+      taskList = deleteTask(taskList);
       break;
-
     case 4:
-      console.log("Obter todas as tarefas");
-      console.log(arrayTarefas);
-      //fazer a função com exibição melhorada
+        listAllTasks(taskList);
       break;
-
     case 5:
-      const idTarefa = prompt.questionInt(
-        "Digite o ID da tarefa que sera exibida: "
-      );
-      console.log(arrayTarefas.get(idTarefa));
-      //fazer a função com exibição melhorada
+        showOneTask(taskList);
       break;
-
     default:
       console.log("Opção inválida. Tente novamente.");
   }
@@ -59,76 +47,128 @@ function escolherOpc(opc) {
 
 let option;
 
-const pausa = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const pause = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 do {
   console.log(menuText());
-  option = prompt.questionInt("Escolha uma opcao: ");
-  escolherOpc(option);
-  await pausa(750);
+  option = prompt.questionInt("Escolha uma opção: ");
+  chooseOption(option);
+  await pause(1000);
 } while (option !== 0);
 
-function adicionarTarefa(arrayTarefas) {
-  let title = prompt.question("Digite o titulo da Tarefa: ");
+// FUNÇÕES DE MANIPULAÇÃO DAS TAREFAS
 
-  while (verificarTitulo(arrayTarefas, title)) {
-    console.log(
-      "Uma tarefa com esse título já existe. Por favor, escolha um título diferente."
-    );
-    title = prompt.question("Digite o título da Tarefa: ");
+
+function addTask(taskList) {
+  let title = prompt.question("Digite o Título da Tarefa: ");
+  while (checkTitle(taskList, title)) {
+    console.log("Uma tarefa com esse título já existe. Por favor, escolha um título diferente.");
+    title = prompt.question("Digite o Título da Tarefa: ");
   }
-
-  const description = prompt.question("Digite a descrição da Tarefa: ");
-  const status = prompt.question("Digite o status da Tarefa: ");
-  arrayTarefas.set(id, { title, description, status });
+  const description = prompt.question("Digite a Descrição da Tarefa: ");
+  const status = prompt.question("Digite o Status da Tarefa: ");
+  taskList.set(id, { title, description, status });
   id++;
 }
 
-function verificarTitulo(arrayTarefas, title) {
-  for (let tarefa of arrayTarefas.values()) {
-    if (tarefa.title === title) {
-      return true;
+
+function checkTitle(taskList, title) {
+    for (let task of taskList.values()) {
+      if (task.title === title) {
+        return true;
+      }
     }
-  }
-  return false;
+    return false;
 }
 
-function editarTarefa(arrayTarefas) {
+
+function updateTask(taskList) {
     
-  const tarefa = arrayTarefas.get(prompt.questionInt("Digite o ID da Tarefa: "));
-  let optionEdit;
-  if (tarefa) {
-    do {
-      optionEdit = prompt.question(
-        "O que deseja editar?\n[1] - Titulo\n[2] - Descrição\n[3] - Status \n [4] - Sair\n"
-      )
-      switch (optionEdit) {
-        case '1':
-            const TitleNovo = prompt.question("Digite o novo título da Tarefa: ");
-            tarefa.title = TitleNovo;
-            console.log("Tarefa atualizada com sucesso!");
-          break;
-        case '2':
-            const descriptionNova = prompt.question("Digite a nova descrição da Tarefa: ");
-            tarefa.description = descriptionNova;
-            console.log("Tarefa atualizada com sucesso!");
-          break;
-        case '3':
-            const statusNovo = prompt.question("Digite o novo status da Tarefa: ");
-            tarefa.status = statusNovo;
-            console.log("Tarefa atualizada com sucesso!");
-          break;
-        case '4':
-          console.log("Saindo...");
-          break;
-  
-        default:1
-          console.log("Opção inválida. Tente novamente.");
+    const task = taskList.get(prompt.questionInt("Digite o ID da Tarefa: "));
+    let optionEdit;
 
-          
-        }
-      }while (optionEdit !== '4');
-  } else {
-    console.log("Tarefa não encontrada.");
-  }
+    if (task) {
+      do {
+        optionEdit = prompt.question(
+          "O que deseja editar?\n[1] - Título\n[2] - Descrição\n[3] - Status\n[4] - Sair\n"
+        )
+        switch (optionEdit) {
+          case '1':
+              task.title = prompt.question("Digite o novo título da Tarefa: ");
+              console.log("Tarefa atualizada com sucesso!");
+            break;
+          case '2':
+              task.description = prompt.question("Digite a nova descrição da Tarefa: ");
+              console.log("Tarefa atualizada com sucesso!");
+            break;
+          case '3':
+              task.status = prompt.question("Digite o novo status da Tarefa: ");
+              console.log("Tarefa atualizada com sucesso!");
+            break;
+          case '4':
+            console.log("\nSaindo...\n\n");
+            break;
+    
+          default:
+            console.log("Opção inválida. Tente novamente.");
+          }
+        } while (optionEdit !== '4');
+    } else {
+      console.log("Tarefa não encontrada.");
+    }
 }
+
+
+
+function deleteTask(taskList) {
+  console.log("Excluir Tarefa");
+
+  if (taskList.size === 0) {
+    console.log("Não há tarefas disponíveis para excluir.");
+    return taskList;
+  }
+
+  taskList.forEach((task, id) => {
+    console.log(`ID: ${id}, Título: ${task.title}, Status: ${task.status}`);
+  });
+
+  const taskId = prompt.questionInt("Digite o ID da tarefa que deseja excluir: ");
+
+  if (taskList.has(taskId)) {
+    taskList.delete(taskId);
+    console.log(`Tarefa com ID ${taskId} excluída com sucesso.`);
+  } else {
+    console.log("Tarefa não encontrada.");
+  }
+
+  return taskList;
+}
+
+
+function listAllTasks(taskList) {
+    if (taskList.length === 0) {
+        console.log("Não há tarefas salvas.");
+    } else {
+        console.log("\n=== Tarefas Salvas ===");
+        taskList.forEach((task, id) => {
+            console.log(`ID: ${id}`);
+            console.log(`Título: ${task.title}`);
+            console.log(`Descrição: ${task.description}`);
+            console.log(`Status: ${task.status}`);
+            console.log("---------------------------");
+        });
+    }
+}
+
+function showOneTask(taskList) {
+    const taskId = prompt.questionInt("Digite o ID da tarefa que deseja exibir: ");
+    if (taskList.has(taskId)) {
+        const task = taskList.get(taskId);
+        console.log(`Título: ${task.title}`);
+        console.log(`Descrição: ${task.description}`);
+        console.log(`Status: ${task.status}`);
+    } else {
+        console.log("Tarefa não encontrada.");
+    }
+}
+
